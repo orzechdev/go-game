444483a4dd0343062fa81718367d5c37497ae868
orzechdev <tarasiukdawid@gmail.com>
2019-05-02 23:02:56 +0200

    PWA

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        Vue Game
-      </v-toolbar-title>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-title class="headline text-uppercase">Vue Game</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
@@ -13,6 +11,18 @@
     <v-content>
       <HelloWorld/>
     </v-content>
+
+    <v-bottom-nav :active.sync="bottomNav" :value="true" fixed color="white">
+      <v-btn color="primary" flat value="play">
+        <span class="font-weight-bold">Play</span>
+        <v-icon>play_circle_filled</v-icon>
+      </v-btn>
+
+      <v-btn color="primary" flat value="settings">
+        <span class="font-weight-bold">Settings</span>
+        <v-icon>settings</v-icon>
+      </v-btn>
+    </v-bottom-nav>
   </v-app>
 </template>
 
