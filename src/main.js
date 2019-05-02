f3eaef8f4ba317dae831a05bf0d3ef760d16c208
orzechdev <tarasiukdawid@gmail.com>
2019-05-02 22:04:40 +0200

    Vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App),
