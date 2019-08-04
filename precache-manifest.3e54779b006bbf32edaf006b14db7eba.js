500d2abd1b05543d4bba4ec82f12c318745152cd
orzechdev <tarasiukdawid@gmail.com>
2019-08-04 18:36:08 +0200

    Updates

---
+++ precache-manifest.3e54779b006bbf32edaf006b14db7eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "59f02bb29ec434bea41f",
+    "url": "/js/chunk-vendors.d26b657c.js"
+  },
+  {
+    "revision": "6164cb38c6a39e858eb5",
+    "url": "/js/app.c1053cec.js"
+  },
+  {
+    "revision": "377f98a6752184d0a6bbf3cb4a3b821a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f02bb29ec434bea41f",
+    "url": "/css/chunk-vendors.0512228f.css"
+  },
+  {
+    "revision": "6164cb38c6a39e858eb5",
+    "url": "/css/app.942e5cd4.css"
+  }
+];
