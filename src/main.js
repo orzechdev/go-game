4e35deaafb9334d0888b42922c6b6226ea5d261a
orzechdev <tarasiukdawid@gmail.com>
2019-08-04 13:11:45 +0200

    Add vuex to handle global state

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import './plugins/vuetify'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
