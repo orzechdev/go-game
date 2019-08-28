01cea599f860aa7ad7d8a3275ce7a3ac995da901
orzechdev <tarasiukdawid@gmail.com>
2019-08-29 00:09:02 +0200

    Revert "Updates"
    This reverts commit a6586ed016c4d6525fb2c11a41a89b28f9d01350.

---
+++ precache-manifest.7d0e94aad900bf655b2bac41bae5c07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/go-game/robots.txt"
+  },
+  {
+    "revision": "59f02bb29ec434bea41f",
+    "url": "/go-game/js/chunk-vendors.d26b657c.js"
+  },
+  {
+    "revision": "9ef3cebb666d8d64bb7e",
+    "url": "/go-game/js/app.dfea3f5b.js"
+  },
+  {
+    "revision": "3a3aad7dfc0bcfab4191d8440e782827",
+    "url": "/go-game/index.html"
+  },
+  {
+    "revision": "59f02bb29ec434bea41f",
+    "url": "/go-game/css/chunk-vendors.0512228f.css"
+  },
+  {
+    "revision": "9ef3cebb666d8d64bb7e",
+    "url": "/go-game/css/app.942e5cd4.css"
+  },
+  {
+    "revision": "e97c5f65dd692ca783b14d89589997db",
+    "url": "/go-game/.well-known/assetlinks.json"
+  }
+];
