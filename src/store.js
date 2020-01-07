a0ef2d4e0eaea540c5e54b4dc940bad00aaf3da2
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-01-07 23:17:23 +0100

    Set min-max as default algorithm

--- src/store.js
+++ src/store.js
@@ -1,65 +1,68 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import {
-  algorithmsTypes,
-  playerMode
-} from "@/components/BoardController.vue";
+import { algorithmsTypes, playerMode } from "@/components/BoardController.vue";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     developerOptions: false,
     playerMode: playerMode.PLAYER_COMPUTER,
     boardSizeSelected: 5,
-    algorithmsTypeSelected: algorithmsTypes.MIN_MAX_A_B,
+    algorithmsTypeSelected: algorithmsTypes.MIN_MAX,
     calculationDepthSelected: 2,
-    boardValues: Array.from({
-      length: 5
-    }, () => Array(5).fill(0)),
+    boardValues: Array.from(
+      {
+        length: 5
+      },
+      () => Array(5).fill(0)
+    )
   },
   mutations: {
     SET_DEVELOPER_OPTIONS(state, isOn) {
-      state.developerOptions = isOn
+      state.developerOptions = isOn;
     },
     SET_PLAYER_MODE(state, mode) {
-      state.playerMode = mode
+      state.playerMode = mode;
     },
     SET_BOARD_SIZE(state, size) {
-      state.boardSizeSelected = size
-      state.boardValues = Array.from({
-        length: size
-      }, () => Array(size).fill(0))
+      state.boardSizeSelected = size;
+      state.boardValues = Array.from(
+        {
+          length: size
+        },
+        () => Array(size).fill(0)
+      );
     },
     SET_BOARD_VALUES(state, values) {
-      state.boardValues = values
+      state.boardValues = values;
     },
     SET_ALGORITHM_TYPE(state, type) {
-      state.algorithmsTypeSelected = type
+      state.algorithmsTypeSelected = type;
     },
     SET_CALCULATION_DEPTH(state, depth) {
-      state.calculationDepthSelected = depth
-    },
+      state.calculationDepthSelected = depth;
+    }
   },
   actions: {
     setDeveloperOptions(context, isOn) {
-      context.commit('SET_DEVELOPER_OPTIONS', isOn);
+      context.commit("SET_DEVELOPER_OPTIONS", isOn);
     },
     setPlayerMode(context, mode) {
-      context.commit('SET_PLAYER_MODE', mode);
+      context.commit("SET_PLAYER_MODE", mode);
     },
     setBoardSizeSelected(context, size) {
-      context.commit('SET_BOARD_SIZE', size);
+      context.commit("SET_BOARD_SIZE", size);
     },
     setBoardValues(context, values) {
-      context.commit('SET_BOARD_VALUES', values);
+      context.commit("SET_BOARD_VALUES", values);
     },
     setAlgorithmsTypeSelected(context, type) {
-      context.commit('SET_ALGORITHM_TYPE', type);
+      context.commit("SET_ALGORITHM_TYPE", type);
     },
     setCalculationDepthSelected(context, depth) {
-      context.commit('SET_CALCULATION_DEPTH', depth);
+      context.commit("SET_CALCULATION_DEPTH", depth);
     }
   }
-})
+});
